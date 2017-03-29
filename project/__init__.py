69ada1f55ecba289afd46e3c65309a3b57b69ab2
Haddon.Sawatzky <haddon.sawatzky@student.ufv.ca>
2017-03-28 20:02:04 -0700

    stuff

--- project/__init__.py
+++ project/__init__.py
@@ -33,8 +33,7 @@ def out():
 def login():
     error = None
     if request.method == 'POST':
-        if request.form['username'] != 'admin' or 
-		request.form['password'] != 'admin':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid username/password. Please try again.'
         else:
             session['logged_in'] = True
