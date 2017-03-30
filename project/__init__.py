611b8c9feb4f55d8703a61c26f06b76c2d5bbaf6
Haddon.Sawatzky <haddon.sawatzky@student.ufv.ca>
2017-03-29 20:52:23 -0700

    changes

--- project/__init__.py
+++ project/__init__.py
@@ -56,7 +56,7 @@ def posts():
         name = request.form['name']
         comment = request.form['comment']
         models.insertPost(name, comment)
-        return render_template('posts.html', posts=posts)
+        return render_template('posts.html')
     else:
         posts = models.retrievePosts()
         return render_template('posts.html', posts=posts)
