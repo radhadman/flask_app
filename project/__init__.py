ec04c75162250a26d2e3574d36f6754705f05423
Haddon.Sawatzky <haddon.sawatzky@student.ufv.ca>
2017-04-05 09:06:14 -0700

    transfer

--- project/__init__.py
+++ project/__init__.py
@@ -1,7 +1,70 @@
-from flask import Flask, Response
+import models
+from flask import Flask, Response, render_template, redirect, url_for, request, session, flash
+from functools import wraps
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return Response("Hello World!"), 200
+# config
+app.secret_key = 'assign2key'
+
+# login decorator
+def login_required(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            return redirect(url_for('login'))
+    return wrap
+	
+	
+@app.route('/')
+@login_required
+def home():
+	return render_template('index.html')	
+	
+
+@app.route('/out')
+def out():
+    return render_template('logout.html')
+
+	
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid username/password. Please try again.'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+	
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('logged_in', None)
+    flash('You have been logged out.')
+    return redirect(url_for('out'))
+	
+	
+@app.route('/posts', methods=['POST', 'GET'])
+def posts():
+    if request.method == 'POST':
+        name = request.form['name']
+        comment = request.form['comment']
+        models.insertPost(name, comment)
+        return render_template('posts.html')
+    else:
+        posts = models.retrievePosts()
+        return render_template('posts.html', posts=posts)
+			
+
+# start the server with the 'run()' method
+if __name__ == '__main__':
+    app.run(debug=True)
+	
+	
+
+        
---
+++ project/models.py
@@ -0,0 +1,30 @@
+import sqlite3 as sql
+
+q = """
+CREATE TABLE IF NOT EXISTS posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    comment TEXT NOT NULL
+);
+"""
+
+con = sql.connect("database.db")
+cur = con.cursor()
+cur.execute(q)
+
+# adapted from: https://gist.github.com/PolBaladas/07bfcdefb5c1c57cdeb5
+
+def insertPost(name,comment):
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    cur.execute("INSERT INTO posts (name,comment) VALUES (?,?)",(name,comment))
+    con.commit()
+    con.close()
+
+def retrievePosts():
+    con = sql.connect("database.db")
+    cur = con.cursor()
+    cur.execute("SELECT name, comment FROM posts")
+    posts = cur.fetchall()
+    con.close()
+    return posts
