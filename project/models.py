d870bf4f8b7fc7c8b92875e7d7f6544d510d7dbb
Haddon.Sawatzky <haddon.sawatzky@student.ufv.ca>
2017-04-06 16:29:43 -0700

    stuff

--- project/models.py
+++ project/models.py
@@ -40,5 +40,10 @@ def retrievePosts():
     con.close()
     return posts
 	
-
+def deletePost():
+	con = sql.connect("database.db")
+	cur = con.cursor()
+	cur.execute("DELETE FROM posts (name,comment) VALUES (?,?)", (name,comment))
+	con.commit()
+    con.close()
 	
